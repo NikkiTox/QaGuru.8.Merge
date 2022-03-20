0db83f84ae6252caf125d48f110601235ca6ffae
Nikita Miroshnichenko <Niki.tone@yandex.ru>
2022-03-21 00:00:21 +0500

    second commit

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTest {
 
     @Test
-     void assertTrueTests(){
-        assertTrue(3>2);
+    void assertTrueTests() {
+        assertTrue(3 > 2);
+    }
+    @Test
+    void assertFalseTests(){
+        assertFalse(3<2);
     }
 }
+
